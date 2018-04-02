5f1bb7e3a93b7ca8a4e29d5be78e4ac579fc6731
Mori Chiharu <chiharu.mori@jp.ricoh.com>
2018-04-02 15:11:34 +0900

    LevelTestクラスの作成

---
+++ source/ForestTest/LevelTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Xunit;
+using Forest;
+
+namespace ForestTest
+{
+
+    public class LevelTest
+    {
+        [Theory]
+        [InlineData(0, "初級者")]
+        [InlineData(1, "中級者")]
+        [InlineData(2, "上級者")]
+        public void ToStringTest(int levelNum, string expectedText)
+        {
+            Level level = new Level { LevelNum = levelNum };
+            Assert.Equal(expectedText, level.ToString());
+        }
+    }
+}
