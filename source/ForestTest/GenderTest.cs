380115f3743423aca56cc520385cd8147b550866
Mori Chiharu <chiharu.mori@jp.ricoh.com>
2018-04-02 15:11:06 +0900

    GenderTestクラスの作成

---
+++ source/ForestTest/GenderTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using Forest;
+
+namespace ForestTest
+{
+    public class GenderTest
+    {
+        /// <summary>
+        /// ToStringメソッドのテスト
+        /// </summary>
+        [Theory]
+        [InlineData(0, "男")]
+        [InlineData(1, "女")]
+        public void ToStringTest(int genderNum, string expectedText)
+        {
+            Gender gender = new Gender { GenderNum = genderNum };
+            Assert.Equal(expectedText, gender.ToString());
+        }
+    }
+}
