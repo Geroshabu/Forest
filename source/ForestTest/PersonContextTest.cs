56e9b578d7b599bfef8a516b494e2ac8c5bdf75d
Mori Chiharu <chiharu.mori@jp.ricoh.com>
2018-04-03 17:04:13 +0900

    属性の追加、AAAパターンに関するコメントの追記

--- source/ForestTest/PersonContextTest.cs
+++ source/ForestTest/PersonContextTest.cs
@@ -28,69 +28,78 @@ namespace ForestTest
             File.Delete("./ForestTest.pdb");
         }
 
-        /// <summary>
-        /// 引数なしコンストラクタを呼び出したときに例外が発生しないことを確認するテスト
-        /// </summary>
-        [Fact]
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "引数なしコンストラクタを呼び出したときに例外が発生しないことを確認する")]
         public void ConstructorTest01()
         {
+            //Arrange：特になし
+
+            //Act
             var personContext = new PersonContext();
+
+            //Assert：特になし
         }
 
-        /// <summary>
-        /// 引数ありコンストラクタを呼び出したときに例外が発生しないことを確認するテスト
-        /// </summary>
-        [Fact]
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "引数ありコンストラクタを呼び出したときに例外が発生しないことを確認する")]
         public void ConstructorTest02()
         {
+            //Arrange
             var connection = new SqliteConnection("DataSource=:memory:");
             connection.Open();
             var options = new DbContextOptionsBuilder<PersonContext>().UseSqlite(connection).Options;
+
+            //Act
             var personContext = new PersonContext(options);
 
+            //Assert：特になし
+
+            connection.Close();
         }
 
-        /// <summary>
-        /// 引数ありコンストラクタに渡された設定情報が不適切である場合に
-        /// SqliteExceptionを投げることを確認するテスト
-        /// </summary>
-        [Fact]
+        [Trait("分類", "異常系")]
+        [Fact(DisplayName = "設定情報が不適切である場合に例外を投げることを確認する")]
         public void ConstructorTest03()
         {
-            Assert.Throws<SqliteException>(delegate
+            //Arrange
+            //存在しないディレクトリを指定
+            var connection = new SqliteConnection("DataSource=/a/damy");
+
+            //Act
+            //Assert
+            Assert.Throws<SqliteException>(() =>
             {
-                //存在しないディレクトリを指定
-                var connection = new SqliteConnection("DataSource=/a/damy");
                 connection.Open();
-                var options = new DbContextOptionsBuilder<PersonContext>().UseSqlite(connection).Options;
-                var personContext = new PersonContext(options);
             });
 
         }
 
-        /// <summary>
-        /// DBの設定を指定しなかったときに想定通りの場所にDBが作られていることを確認するテスト
-        /// </summary>
-        [Fact]
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = " DBの設定を指定しなかったときに想定通りの場所にDBが作られていることを確認する")]
         public void ConstructorTest04()
         {
+            //Arrange：特になし
+
+            //Act
             //DBを作成
             var personContext = new PersonContext();
+
+            //Assert
             //現在の場所を取得
             var fullPath = Path.GetFullPath(".");
             //現在の場所にテスト用のDBが作られているかを確認
             Assert.True(File.Exists(fullPath + "/ForestTest.pdb"));
         }
 
-        /// <summary>
-        /// DBの設定を指定したときに指定通りの場所にDBが作られていることを確認するテスト
-        /// </summary>
-        [Fact]
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "DBの設定を指定したときに指定通りの場所にDBが作られていることを確認する")]
         public void ConstructorTest05()
         {
+            //Arrange
             //作成するDBの名前
             string dbName = "/test.db";
 
+            //Act
             //DBを作成
             var connection = new SqliteConnection("DataSource=" + DirectoryName + dbName);
             connection.Open();
@@ -98,6 +107,7 @@ namespace ForestTest
             var personContext = new PersonContext(options);
             connection.Close();
 
+            //Assert
             //指定場所に指定した名前のDBファイルがあるかを確認
             Assert.True(File.Exists(DirectoryName + dbName));
 
