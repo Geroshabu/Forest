e598f46665d79be5560fa27aea6170b53b4037d8
Mori Chiharu <chiharu.mori@jp.ricoh.com>
2018-04-09 17:03:59 +0900

    PersonHolderとテストクラスの作成（メンバー追加・削除・変更は未実装）

---
+++ source/Forest/PersonHolder.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forest
+{
+    public class PersonHolder
+    {
+        //DBに登録されているサークルメンバーを保持しておくリスト
+        public List<Person> Persons { get; set; }
+
+        public PersonHolder(List<Person> persons)
+        {
+            //引数のリストをそのままPersonsにコピー
+            Persons = new List<Person>();
+            foreach(var target in persons)
+            {
+                Persons.Add(target);
+            }
+        }
+
+        /// <summary>
+        /// 引数のIDをもつ人の参加フラグを立てる
+        /// </summary>
+        /// <param name="id_ist">参加者として選択されている人のID</param>
+        public void Attend(List<string> id_list)
+        {
+            //IDリストをはじめから見ていく
+            foreach (string targetID in id_list)
+            {
+                //リストの中にいる一致するIDの人を探す
+                foreach (Person targetPerson in Persons)
+                {
+                    //一致するIDの人の参加フラグを立てる
+                    if (targetPerson.ID == targetID)
+                    {
+                        targetPerson.AttendFlag = true;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 引数のIDをもつ人の参加フラグを下げる
+        /// </summary>
+        /// <param name="id_list">参加を取り下げた人のID</param>
+        public void Cancel(List<string> id_list)
+        {
+            //IDリストをはじめから見ていく
+            foreach (string targetID in id_list)
+            {
+                //リストの中にいる一致するIDの人を探す
+                foreach (Person targetPerson in Persons)
+                {
+                    //一致するIDの人の参加フラグを下げる
+                    if (targetPerson.ID == targetID)
+                    {
+                        targetPerson.AttendFlag = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 【未実装】
+        /// </summary>
+        /// <param name="new_person">新しく追加された人の情報</param>
+        public void Add(Person new_person)
+        {
+            
+        }
+
+        /// <summary>
+        /// 【未実装】
+        /// </summary>
+        /// <param name="id_list">削除する人のIDリスト</param>
+        public void Delete(List<string> id_list)
+        {
+
+        }
+
+        /// <summary>
+        /// 【未実装】
+        /// </summary>
+        /// <param name="new_person">更新後の情報</param>
+        public void update(Person new_person)
+        {
+
+        }
+
+        /// <summary>
+        /// DBに登録されているメンバーの中で削除されていないメンバー全員を返す
+        /// </summary>
+        /// <returns>削除されていないメンバー全員のリスト</returns>
+        public List<Person> GetAllPersons()
+        {
+            //削除されていないメンバー全員
+            var notDeletedPersons = new List<Person>();
+
+            foreach (Person target in Persons)
+            {
+                //削除フラグが立っていなければリストに追加
+                if(target.DeleteFlag == false)
+                {
+                    notDeletedPersons.Add(target);
+                }
+            }
+
+            return notDeletedPersons;
+        }
+
+        /// <summary>
+        /// 削除されていないメンバーの中でも練習に参加するメンバーを返す
+        /// </summary>
+        /// <returns>削除されていない、かつ練習に参加するメンバーのリスト</returns>
+        public List<Person> GetAttendedPersons()
+        {
+            //削除されていないかつ練習に参加するメンバー全員
+            var attendedPersons = new List<Person>();
+
+            //削除されていないメンバーの中から参加フラグが立っているメンバーをリストに追加
+            foreach(Person target in GetAllPersons())
+            {
+                if(target.AttendFlag == true)
+                {
+                    attendedPersons.Add(target);
+                }
+            }
+
+            return attendedPersons;
+        }
+
+        
+
+    }
+}
---
+++ source/ForestTest/PersonHolderTest.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Forest;
+using Xunit;
+
+namespace ForestTest
+{
+    public class PersonHolderTest
+    {
+        List<Person> persons;
+
+        /// <summary>
+        /// PersonHplderに登録しておくためのデータを用意する
+        /// </summary>
+        /// <returns>データのリスト</returns>
+        public List<Person> PrepareData01()
+        {
+            persons = new List<Person>();
+
+            var men = new Gender { GenderNum = 0 };
+            var women = new Gender { GenderNum = 1 };
+            var beginner = new Level { LevelNum = 0 };
+            var intermediate = new Level { LevelNum = 1 };
+            var advanced = new Level { LevelNum = 2 };
+
+            var testPerson01 = new Person
+            {
+                ID = "test01",
+                Name = "snoopy",
+                Gender = men,
+                Level = advanced,
+                DeleteFlag = false,
+                AttendFlag = false
+            };
+            var testPerson02 = new Person
+            {
+                ID = "test02",
+                Name = "bell",
+                Gender = women,
+                Level = beginner,
+                DeleteFlag = false,
+                AttendFlag = true
+            };
+            var testPerson03 = new Person
+            {
+                ID = "test03",
+                Name = "andy",
+                Gender = men,
+                Level = intermediate,
+                DeleteFlag = true,
+                AttendFlag = false
+            };
+            var testPerson04 = new Person
+            {
+                ID = "test04",
+                Name = "marbles",
+                Gender = men,
+                Level = advanced,
+                DeleteFlag = true,
+                AttendFlag = true
+            };
+            var testPerson05 = new Person
+            {
+                ID = "test05",
+                Name = "spike",
+                Gender = men,
+                Level = beginner,
+                DeleteFlag = false,
+                AttendFlag = false
+            };
+            var testPerson06 = new Person
+            {
+                ID = "test06",
+                Name = "olaf",
+                Gender = men,
+                Level = intermediate,
+                DeleteFlag = false,
+                AttendFlag = true
+            };
+
+            persons.Add(testPerson01);
+            persons.Add(testPerson02);
+            persons.Add(testPerson03);
+            persons.Add(testPerson04);
+            persons.Add(testPerson05);
+            persons.Add(testPerson06);
+
+            return persons;
+        }
+
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "参加フラグを正常に立てられることを確認する")]
+        public void AttendTest01()
+        {
+            //Arrange
+            //データの準備
+            var personHolder = new PersonHolder(PrepareData01());
+            //参加数メンバーのIDをリストにセット
+            var idList = new List<string>();
+            idList.Add("test01");
+            idList.Add("test05");
+
+            //Act
+            //実行
+            personHolder.Attend(idList);
+
+            //Assert
+            Assert.True(personHolder.Persons.Where(x => x.ID == "test01").FirstOrDefault().AttendFlag);
+            Assert.True(personHolder.Persons.Where(x => x.ID == "test02").FirstOrDefault().AttendFlag);
+            Assert.False(personHolder.Persons.Where(x => x.ID == "test03").FirstOrDefault().AttendFlag);
+            Assert.True(personHolder.Persons.Where(x => x.ID == "test04").FirstOrDefault().AttendFlag);
+            Assert.True(personHolder.Persons.Where(x => x.ID == "test05").FirstOrDefault().AttendFlag);
+            Assert.True(personHolder.Persons.Where(x => x.ID == "test06").FirstOrDefault().AttendFlag);
+
+        }
+
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "参加を正常に取り消せることを確認する")]
+        public void Cancelest01()
+        {
+            //Arrange
+            //データの準備
+            var personHolder = new PersonHolder(PrepareData01());
+            //参加を取り消すメンバーのIDをリストにセット
+            var idList = new List<string>();
+            idList.Add("test02");
+            idList.Add("test06");
+
+            //Act
+            //実行
+            personHolder.Cancel(idList);
+
+            //Assert
+            Assert.False(personHolder.Persons.Where(x => x.ID == "test01").FirstOrDefault().AttendFlag);
+            Assert.False(personHolder.Persons.Where(x => x.ID == "test02").FirstOrDefault().AttendFlag);
+            Assert.False(personHolder.Persons.Where(x => x.ID == "test03").FirstOrDefault().AttendFlag);
+            Assert.True(personHolder.Persons.Where(x => x.ID == "test04").FirstOrDefault().AttendFlag);
+            Assert.False(personHolder.Persons.Where(x => x.ID == "test05").FirstOrDefault().AttendFlag);
+            Assert.False(personHolder.Persons.Where(x => x.ID == "test06").FirstOrDefault().AttendFlag);
+
+        }
+
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "削除されていないメンバーを取得できることを確認する")]
+        public void GetAllPersonsTest01()
+        {
+            //Arrange
+            //データの準備
+            var personHolder = new PersonHolder(PrepareData01());
+
+            //Act
+            //実行
+            var actual = personHolder.GetAllPersons();
+
+            //Assert
+            Assert.Equal(PrepareData01().Where(x => x.DeleteFlag == false).Count(), actual.Count);
+
+        }
+
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "メンバーがいないときに空のリストを返すことを確認する")]
+        public void GetAllPersonsTest02()
+        {
+            //Arrange
+            //データの準備
+            var emptyPersons = new List<Person>();
+            var personHolder = new PersonHolder(emptyPersons);
+
+            //Act
+            //実行
+            var actual = personHolder.GetAllPersons();
+
+            //Assert
+            Assert.Empty(actual);
+
+        }
+
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "参加フラグが立っているメンバーを取得できることを確認する")]
+        public void GetAttendedPersonsTest01()
+        {
+            //Arrange
+            //データの準備
+            var personHolder = new PersonHolder(PrepareData01());
+
+            //Act
+            //実行
+            var actual = personHolder.GetAttendedPersons();
+
+            //Assert
+            Assert.Equal(PrepareData01().Where(x => (x.DeleteFlag == false)&&(x.AttendFlag == true)).Count(), actual.Count);
+
+        }
+
+        [Trait("分類", "正常系")]
+        [Fact(DisplayName = "メンバーがいないときに空のリストを返すことを確認する")]
+        public void GetAttendedPersonsTest02()
+        {
+            //Arrange
+            //データの準備
+            var emptyPersons = new List<Person>();
+            var personHolder = new PersonHolder(emptyPersons);
+
+            //Act
+            //実行
+            var actual = personHolder.GetAttendedPersons();
+
+            //Assert
+            Assert.Empty(actual);
+
+        }
+
+
+
+
+    }
+}
