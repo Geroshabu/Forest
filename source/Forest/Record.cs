cee08c86b55da013cd3fa92bfca0f0b7bfefe5bb
Mori Chiharu <chiharu.mori@jp.ricoh.com>
2018-06-07 14:46:22 +0900

    Recordクラスを作成した

---
+++ source/Forest/Record.cs
@@ -0,0 +1,26 @@
+﻿namespace Forest
+{
+    /// <summary>
+    /// 対戦履歴
+    /// </summary>
+    public class Record
+    {
+        public Record(Person player1,Person player2)
+        {
+            Opponent = new Person[2];
+            Opponent[0] = player1;
+            Opponent[1] = player2;
+            Count = 1;
+        }
+
+        /// <summary>
+        /// 対戦した組み合わせ
+        /// </summary>
+        public Person[] Opponent { get; }
+
+        /// <summary>
+        /// 戦った回数
+        /// </summary>
+        public int Count { get; set; }
+    }
+}
