bd499b3d72b56bec90cf81b61f833cc83b22055c
Mori Chiharu <chiharu.mori@jp.ricoh.com>
2018-06-07 16:47:48 +0900

    同様に、いったん退避

--- source/Forest/GameGeneratorFactory.cs
+++ source/Forest/GameGeneratorFactory.cs
@@ -2,6 +2,11 @@
 {
     class GameGeneratorFactory : IGameGeneratorFactory
     {
+        /// <summary>
+        /// 引数に合ったIGameGeneratorを作る
+        /// </summary>
+        /// <param name="generateMode">現在指定されている組み合わせ方法</param>
+        /// <returns>指定された組み合わせ方法のGenerator</returns>
         public IGameGenerator Create(int generateMode)
         {
             switch (generateMode)
@@ -9,7 +14,7 @@
                 case 0:
                     return new RandomGenerator();
 
-                default:
+                default: //デフォルトは完全ランダム
                     return new RandomGenerator();
             }
         }
