4cb2486b01c092af47bd91ab7b619e76c1141209
Mori Chiharu <chiharu.mori@jp.ricoh.com>
2018-06-13 13:55:20 +0900

    リストをシャッフルするメソッドをリストを加工するクラス内に作成

---
+++ source/Forest/ProcessList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forest
+{
+    /// <summary>
+    /// リストの加工をする処理をまとめたクラス
+    /// </summary>
+    public class ProcessList
+    {
+        /// <summary>
+        /// 引数のリストをシャッフルする
+        /// </summary>
+        /// <param name="targetList">シャッフルしたいリスト</param>
+        /// <returns>シャッフル後のリスト</returns>
+        public static List<Person> Shuffle(List<Person> targetList)
+        {
+            var resultList = new List<Person>();//結果のリスト
+            var copyList = new List<Person>(targetList);//元のリストのコピー
+            var random = new Random();
+            //元のリストのコピー（ループ用のリスト）がなくなったらおしまい
+            while (copyList.Count != 0)
+            {
+                //ランダムで選んだものを入れていく
+                int j = random.Next(copyList.Count);
+                resultList.Add(copyList[j]);
+                copyList.RemoveAt(j);
+            }
+            return resultList;
+        }
+
+    }
+}
