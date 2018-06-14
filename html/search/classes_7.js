89748e3228b124594cd2493649b9bba70ee79981
Geroshabu <oonstks@gmail.com>
2018-06-13 23:25:44 -0700

    auto commit doxygen products by AppVeyor
    53e311f57d23ea5b18826f4e34cb59e6e8bb75d8

--- html/search/classes_7.js
+++ html/search/classes_7.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['randombygendergenerator',['RandomByGenderGenerator',['../class_forest_1_1_random_by_gender_generator.html',1,'Forest']]],
+  ['randombylevelgenerator',['RandomByLevelGenerator',['../class_forest_1_1_random_by_level_generator.html',1,'Forest']]],
   ['randomgenerator',['RandomGenerator',['../class_forest_1_1_random_generator.html',1,'Forest']]],
   ['record',['Record',['../class_forest_1_1_record.html',1,'Forest']]],
   ['resources',['Resources',['../class_forest_1_1_properties_1_1_resources.html',1,'Forest::Properties']]]
